7a7e57a1dcc094dd5b4baad198be79d9704181fb
Konstantin Winkel <konstantin.winkel@stud-mail.uni-wuerzburg.de>
2024-04-23 11:45:39 +0200

    Header was forgotten

--- include/controller.h
+++ include/controller.h
@@ -15,17 +15,37 @@ class Controller {
         Controller(Object * obj);
 
         virtual void initializeController() = 0;
-        virtual void applyControlForce(long double timestamp) = 0;
+        virtual void applyControlForce(long double timestamp, long double delta_t) = 0;
         virtual void finalizeController() = 0;
 };
 
-class LinearController: public Controller {
+class TimeIntervalController : public Controller {
     public:
-        LinearController(Object * obj);
+        enum Direction {
+            Prograde,
+            Retrograde
+        };
+    
+    private:
+        struct Interval {
+            Direction dir;
+            long double start_time;
+            long double end_time;
+        };
+
+        std::vector<Interval> intervals;
+
+        long double force;
+
+    public:
+
+        TimeIntervalController(Object * obj, long double force);
 
         void initializeController() override;
-        void applyControlForce(long double timestamp) override;
+        void applyControlForce(long double timestamp, long double delta_t) override;
         void finalizeController() override;
+
+        void addInterval(Direction dir, long double start_time, long double end_time);
 };
 
 #endif
